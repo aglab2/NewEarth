972757fc8bf447b2c825b1eb7f328f56f371f7bc
aglab2 <aglab3@gmail.com>
2023-08-27 23:25:08 +0800

    Fix JRB water texture

--- levels/jrb/texscroll.inc.c
+++ levels/jrb/texscroll.inc.c
@@ -19,6 +19,27 @@ void scroll_jrb_dl_DL_jrb_1_0xe016a40_Obj_mesh_layer_1_vtx_5() {
 	currentY += deltaY;
 }
 
+void scroll_jrb_dl_DL_jrb_1_0xe01a8f8_Obj_mesh_layer_5_vtx_0() {
+	int i = 0;
+	int count = 8;
+	int height = 32 * 0x20;
+
+	static int currentY = 0;
+	int deltaY;
+	Vtx *vertices = segmented_to_virtual(jrb_dl_DL_jrb_1_0xe01a8f8_Obj_mesh_layer_5_vtx_0);
+
+	deltaY = (int)(0.2200000286102295 * 0x20) % height;
+
+	if (absi(currentY) > height) {
+		deltaY -= (int)(absi(currentY) / height) * height * signum_positive(deltaY);
+	}
+
+	for (i = 0; i < count; i++) {
+		vertices[i].n.tc[1] += deltaY;
+	}
+	currentY += deltaY;
+}
+
 void scroll_jrb_dl_DL_jrb_2_0xe006d10_Obj_mesh_layer_1_vtx_3() {
 	int i = 0;
 	int count = 20;
@@ -63,6 +84,7 @@ void scroll_jrb_dl_DL_jrb_4_0xe002c50_Obj_mesh_layer_1_vtx_2() {
 
 void scroll_jrb() {
 	scroll_jrb_dl_DL_jrb_1_0xe016a40_Obj_mesh_layer_1_vtx_5();
+	scroll_jrb_dl_DL_jrb_1_0xe01a8f8_Obj_mesh_layer_5_vtx_0();
 	scroll_jrb_dl_DL_jrb_2_0xe006d10_Obj_mesh_layer_1_vtx_3();
 	scroll_jrb_dl_DL_jrb_4_0xe002c50_Obj_mesh_layer_1_vtx_2();
 };
--- levels/jrb/texscroll.inc.h
+++ levels/jrb/texscroll.inc.h
@@ -1,4 +1,5 @@
 extern void scroll_jrb_dl_DL_jrb_1_0xe016a40_Obj_mesh_layer_1_vtx_5();
+extern void scroll_jrb_dl_DL_jrb_1_0xe01a8f8_Obj_mesh_layer_5_vtx_0();
 extern void scroll_jrb_dl_DL_jrb_2_0xe006d10_Obj_mesh_layer_1_vtx_3();
 extern void scroll_jrb_dl_DL_jrb_4_0xe002c50_Obj_mesh_layer_1_vtx_2();
 extern void scroll_jrb();
