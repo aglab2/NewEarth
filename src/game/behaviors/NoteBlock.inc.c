9719ea13cb64ad4f7b52543921e85ee260474cfb
aglab2 <aglab3@gmail.com>
2023-08-27 19:38:59 +0800

    Fixed noteblock

--- src/game/behaviors/NoteBlock.inc.c
+++ src/game/behaviors/NoteBlock.inc.c
@@ -1,15 +1,14 @@
 void bhv_noteblock_loop(void) {
-	f32 Yspd = 64.0f;
-	u32 CarmackMeme;
 	if (cur_obj_is_mario_on_platform()) {
-		//this is awful
-		if (gMarioStates[0].controller->buttonPressed & A_BUTTON) {
-			Yspd = 200;
-		}
 		set_mario_action(gMarioState, ACT_DOUBLE_JUMP, 0);
-		CarmackMeme = *(u32 *) &Yspd;
-		CarmackMeme += (o->oBehParams2ndByte << 16);
-		Yspd = *(f32 *) &CarmackMeme;
+		f32 Yspd = 200.0f;
+		if (gMarioStates[0].controller->buttonDown & A_BUTTON) {
+            int magic = 0x42800000 + 0x10000 * GET_BPARAM2(o->oBehParams);
+            Yspd = *(float*)&magic;
+		}
+		if (gMarioStates[0].controller->buttonPressed & B_BUTTON) {
+			Yspd = 0.f;
+		}
 		gMarioStates[0].vel[1] = Yspd;
 		o->oAction = 1;
 		// gMarioStates[0].flags |= MARIO_NO_FALL_DAMAGE;
