b738d2191a3b682a599da67997cb0178496349af
aglab2 <aglab3@gmail.com>
2023-08-28 08:09:43 +0800

    Fix B1 performance

--- levels/bitdw/texscroll.inc.c
+++ levels/bitdw/texscroll.inc.c
@@ -21,7 +21,7 @@ void scroll_bitdw_dl_DL_bitdw_1_0xe03f170_Obj_mesh_layer_1_vtx_0() {
 
 void scroll_bitdw_dl_z_mesh_layer_1_vtx_0() {
 	int i = 0;
-	int count = 2173;
+	int count = 275;
 	int height = 32 * 0x20;
 
 	static int currentY = 0;
