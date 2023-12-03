b05a232da493fe70e451ceb0ed38ee152bf67351
aglab2 <aglab3@gmail.com>
2023-12-03 19:25:09 +0800

    Mark b1 moving pyramids unsafe

--- actors/square_platform/collision.inc.c
+++ actors/square_platform/collision.inc.c
@@ -8,7 +8,7 @@ const Collision bitdw_seg7_collision_moving_pyramid[] = {
     COL_VERTEX(307, 0, 307),
     COL_VERTEX(0, -306, 0),
     COL_VERTEX(-306, 0, 307),
-    COL_TRI_INIT(SURFACE_DEFAULT, 8),
+    COL_TRI_INIT(SURFACE_0004, 8),
     COL_TRI(0, 1, 2),
     COL_TRI(0, 3, 1),
     COL_TRI(1, 3, 4),
